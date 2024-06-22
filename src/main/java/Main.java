7207cb847d2d8f2a8a30cc2f7d7f12779dbb1850
Marcin Kurek <marcin.kurek@rocker.com>
2024-06-23 01:55:17 +0200

    return a file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,13 +4,19 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 public class Main {
   public static void main(String[] args) {
+      String directory = (args.length >= 2 && "--directory".equals(args[0])) ? args[1] : null;
       ExecutorService executorService = Executors.newFixedThreadPool(64);
       try (ServerSocket serverSocket = new ServerSocket(4221)) {
           serverSocket.setReuseAddress(true);
@@ -18,7 +24,7 @@ public class Main {
               Socket clientSocket = serverSocket.accept();
               executorService.submit(() -> {
                   try {
-                      handleSingleConnection(clientSocket);
+                      handleRequest(clientSocket, directory);
                   } catch (IOException e) {
                       System.out.println("IOException: " + e.getMessage());
                   }
@@ -29,10 +35,10 @@ public class Main {
      }
   }
 
-    private static void handleSingleConnection(Socket clientSocket) throws IOException {
+    private static void handleRequest(Socket clientSocket, String filesDirectory) throws IOException {
         System.out.println("accepted new connection");
         HttpRequest request = readRequest(clientSocket.getInputStream());
-        HttpResponse response = buildResponse(request);
+        HttpResponse response = buildResponse(request, filesDirectory);
         clientSocket.getOutputStream().write(response.toBytes());
     }
 
@@ -49,7 +55,7 @@ public class Main {
         return lines;
     }
 
-    private static HttpResponse buildResponse(HttpRequest request) {
+    private static HttpResponse buildResponse(HttpRequest request, String filesDirectory) throws IOException {
         String[] pathParts = request.path.substring(1).split("/");
         if (request.path.equals("/")) {
             return new HttpResponse(HttpStatusCode.OK, Collections.emptyMap(), null);
@@ -57,6 +63,8 @@ public class Main {
             return userAgentResponse(request.headers.get("user-agent"));
         } else if (pathParts.length == 2 && pathParts[0].equals("echo")) {
             return echoResponse(pathParts[1]);
+        } else if (pathParts.length == 2 && pathParts[0].equals("files")) {
+            return filesResponse(filesDirectory, pathParts[1]);
         } else {
             return new HttpResponse(HttpStatusCode.NOT_FOUND, Collections.emptyMap(), null);
         }
@@ -82,6 +90,25 @@ public class Main {
                 ),
                 userAgentValue
         );
+    }
+
+    private static HttpResponse filesResponse(String filesDirectory, String filename) throws IOException {
+        Path path = Paths.get(filesDirectory + filename);
+        if (!Files.exists(path)) {
+            return new HttpResponse(HttpStatusCode.NOT_FOUND, Collections.emptyMap(), null);
+        } else  {
+            try(Stream<String> lines = Files.lines(path)) {
+                String fileContent = lines.collect(Collectors.joining("\n"));
+                return new HttpResponse(
+                        HttpStatusCode.OK,
+                        Map.of(
+                                "Content-Type", "application/octet-stream",
+                                "Content-Length", String.valueOf(fileContent.length())
+                        ),
+                        fileContent
+                );
+            }
+        }
     }
 
 }
